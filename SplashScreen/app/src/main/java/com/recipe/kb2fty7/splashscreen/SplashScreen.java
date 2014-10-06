4acce9a352d84da26facc3b29ecae696d8e043b7
kb2fty7 <kb2fty7@gmail.com>
2014-10-07 01:06:57 +0300

    #1
    Simple Example how create splash screen in application for android

---
+++ SplashScreen/app/src/main/java/com/recipe/kb2fty7/splashscreen/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.recipe.kb2fty7.splashscreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class SplashScreen extends Activity {
+    private static final int DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },DELAY);
+    }
+
+}
